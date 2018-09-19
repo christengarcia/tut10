4855856dfdc5637388f317a8d44e42ef80d50d2b
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-19 18:13:46 +1000

    Initial commit

---
+++ tut10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Function to total orders and apply any applicable discount
+"""
+# Stock prices
+price = {'book': 10.0, 'magazine': 5.5, 'newspaper': 2.0}
+
+# Separate orders   
+order1 = {'book': 10}
+order2 = {'book': 1, 'magazine': 3}
+order3 = {'magazine': 5, 'book': 10}
