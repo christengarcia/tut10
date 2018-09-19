f217885d8cda16be90a292fa927551f28728d3f8
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-09-19 19:29:35 +1000

    Add function: calculate_price

--- tut10.py
+++ tut10.py
@@ -10,3 +10,31 @@ price = {'book': 10.0, 'magazine': 5.5, 'newspaper': 2.0}
 order1 = {'book': 10}
 order2 = {'book': 1, 'magazine': 3}
 order3 = {'magazine': 5, 'book': 10}
+
+# Calculate total price on orders
+# Add discount to orders if applicable 
+def calculate_price(price, order):
+    order_basket = []
+    for key, value in price.items():
+        # Raise KeyError if item does not have a price
+        if price[key] < 0: 
+            raise KeyError('item in order does not have price value') 
+        # Check if item has a match in stock    
+        if key in order:            
+            gross = float(price[key]) * float(order[key])            
+            order_basket.append(gross)
+        # Sum total on each iteration        
+        gross_total = 0
+        for sub_total in order_basket:
+            gross_total = gross_total + sub_total             
+    # Add discount if applicable
+    if gross_total > 100.00:
+        discount = gross_total * 10 / 100
+        discounted_price = gross_total - discount
+        return discounted_price
+    elif gross_total > 50.00:
+        discount = gross_total * 5 / 100
+        discounted_price = gross_total - discount
+        return discounted_price
+    else:
+        return gross_total
